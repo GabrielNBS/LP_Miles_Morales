49651c13596ccff8e4e1c99cedb6e96c6ef90d37
GabrielNBS <123820384+GabrielNBS@users.noreply.github.com>
2023-07-09 18:58:13 -0300

    add minificacao de video

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,12 @@ function images () {
         .pipe(gulp.dest('./dist/images'));
 }
 
+function videos() {
+    return gulp.src('./src/video/*.mp4')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/video'));
+}
+
 function styles () {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
@@ -22,7 +28,7 @@ function scripts () {
         .pipe(gulp.dest('./dist/js'));
 }
 
-exports.default = gulp.parallel(images, styles, scripts);
+exports.default = gulp.parallel(images, styles, scripts, videos);
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
